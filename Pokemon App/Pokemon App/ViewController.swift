0c3a3f9c7d6e6262b58b62cb3112d7f8d321e771
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-21 20:54:34 +0700

    Create Pokemon App project

---
+++ Pokemon App/Pokemon App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon App
+//
+//  Created by PT.Koanba on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
