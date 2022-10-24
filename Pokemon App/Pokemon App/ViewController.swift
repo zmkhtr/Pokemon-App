a9e2cfa9a20862a15084842d02502a44f88904b7
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-24 18:52:02 +0700

    Revert "Merge pull request #3 from jaygung17/NovanAgungWaskito"
    This reverts commit 4a3acad35feafe3610bbd2660b232991f72e68c5, reversing
    changes made to 1a3f7671dfb4400d00fd49a02dbd1599a392c1be.

--- Pokemon App/Pokemon App/ViewController.swift
+++ Pokemon App/Pokemon App/ViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Login Features
-        
     }
+
 
 }
 
