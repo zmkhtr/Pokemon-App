3c5a8955df3b6a5f8fecf2af54179553cbfb0414
Nadia Darin <darinadia98@gmail.com>
2022-10-25 19:17:00 +0700

    loadViewFromNib function for NetworkImageView

--- Pokemon App/Pokemon App/NetworkImageView/NetworkImageView.swift
+++ Pokemon App/Pokemon App/NetworkImageView/NetworkImageView.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class NetworkImageView: UIView {
 
+    let nibName = "NetworkImageView"
+    var contentView: UIView?
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var retryButton: UIButton!
 
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+        contentView = view
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
 }
