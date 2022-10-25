070710ccd6f76a668b4531be7252d0469f15b914
Nadia Darin <darinadia98@gmail.com>
2022-10-25 18:22:03 +0700

    Add UIImageView to NetworkImageView

--- Pokemon App/Pokemon App/NetworkImageView/NetworkImageView.swift
+++ Pokemon App/Pokemon App/NetworkImageView/NetworkImageView.swift
@@ -9,12 +9,7 @@ import UIKit
 
 class NetworkImageView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var imageView: UIImageView!
+    
 
 }
