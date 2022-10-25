60a0f6f5a76c813899ae7c84437b309b3d965b21
Nadia Darin <darinadia98@gmail.com>
2022-10-25 19:19:42 +0700

    downloadImage function for NetworkImageView

--- Pokemon App/Pokemon App/NetworkImageView/NetworkImageView.swift
+++ Pokemon App/Pokemon App/NetworkImageView/NetworkImageView.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class NetworkImageView: UIView {
-
+    
+    var imageURL = "https://images.pokemontcg.io/swsh4/25_hires.png"
     let nibName = "NetworkImageView"
     var contentView: UIView?
+    let loader = ImageLoader()
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var retryButton: UIButton!
-
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -22,6 +24,7 @@ class NetworkImageView: UIView {
         view.frame = self.bounds
         self.addSubview(view)
         contentView = view
+        downloadImage()
     }
     
     func loadViewFromNib() -> UIView? {
@@ -29,4 +32,40 @@ class NetworkImageView: UIView {
         let nib = UINib(nibName: nibName, bundle: bundle)
         return nib.instantiate(withOwner: self, options: nil).first as? UIView
     }
+    
+    func downloadImage(){
+        startDownloadImage()
+        loader.downloadImageWithURLSession(url: URL(string: imageURL)!) { result in
+            switch result {
+            case .success(let downloadedImage):
+                self.successedDownloadImage(downloadedImage)
+            case .failure:
+                self.failedDownloadImage()
+            }
+        }
+    }
+    
+    private func startDownloadImage(){
+        imageView.isHidden = true
+        errorLabel.isHidden = true
+        retryButton.isHidden = true
+        isShimmering = true
+        backgroundColor = .gray
+    }
+    
+    private func successedDownloadImage(_ img: UIImage) {
+        imageView.isHidden = false
+        imageView.image = img
+        isShimmering = false
+        backgroundColor = nil
+    }
+    
+    private func failedDownloadImage(){
+        print("Error")
+        errorLabel.isHidden = true
+        retryButton.isHidden = true
+        isShimmering = false
+        backgroundColor = nil
+    }
+    
 }
