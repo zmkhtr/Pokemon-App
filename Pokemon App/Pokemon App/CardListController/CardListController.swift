d2eec0520e30f4c94da11e47e24b1eceec5ea21e
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-10-26 10:48:57 +0700

    FN-0 Tab Bar Controller

---
+++ Pokemon App/Pokemon App/CardListController/CardListController.swift
@@ -0,0 +1,29 @@
+//
+//  CardListController.swift
+//  Pokemon App
+//
+//  Created by Novan Agung Waskito on 26/10/22.
+//
+
+import UIKit
+
+class CardListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
