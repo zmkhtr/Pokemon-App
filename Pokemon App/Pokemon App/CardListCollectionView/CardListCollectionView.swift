d530c56c16ce83b0c67735d8fbe9de50639fe714
Nadia Darin <darinadia98@gmail.com>
2022-11-05 21:37:20 +0700

    Create CardListCollectionView

---
+++ Pokemon App/Pokemon App/CardListCollectionView/CardListCollectionView.swift
@@ -0,0 +1,31 @@
+//
+//  CardListCollectionView.swift
+//  Pokemon App
+//
+//  Created by Nadia Darin on 05/11/22.
+//
+
+import UIKit
+
+class CardListCollectionView: UICollectionView {
+    
+   
+        var contentView: UIView?
+
+    required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+
+            guard let view = loadViewFromNib() else { return }
+            view.frame = self.bounds
+            self.addSubview(view)
+            contentView = view
+        }
+
+        func loadViewFromNib() -> UIView? {
+            let bundle = Bundle(for: type(of: self))
+            let nib = UINib(nibName: "CardListCollectionView", bundle: bundle)
+            return nib.instantiate(withOwner: self, options: nil).first as? UIView
+        }
+
+
+}
