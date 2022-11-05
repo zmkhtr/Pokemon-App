7d4d6fe0a96db49bc49bc44810b937aa90c1cd59
Nadia Darin <darinadia98@gmail.com>
2022-11-05 10:34:41 +0700

    Create CardListViewController

---
+++ Pokemon App/Pokemon App/CardListViewController/CardListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CardListViewController.swift
+//  Pokemon App
+//
+//  Created by Nadia Darin on 05/11/22.
+//
+
+import UIKit
+
+class CardListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Login Features
+    }
+
+
+}
