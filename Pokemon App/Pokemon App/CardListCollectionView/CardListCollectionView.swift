18f826eeac01636d2957e143407177996e1753d1
Nadia Darin <darinadia98@gmail.com>
2022-11-05 21:50:05 +0700

    Remove unused collection view xib

--- Pokemon App/Pokemon App/CardListCollectionView/CardListCollectionView.swift
+++ Pokemon App/Pokemon App/CardListCollectionView/CardListCollectionView.swift
@@ -10,22 +10,22 @@ import UIKit
 class CardListCollectionView: UICollectionView {
     
    
-        var contentView: UIView?
+//        var contentView: UIView?
 
-    required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-
-            guard let view = loadViewFromNib() else { return }
-            view.frame = self.bounds
-            self.addSubview(view)
-            contentView = view
-        }
-
-        func loadViewFromNib() -> UIView? {
-            let bundle = Bundle(for: type(of: self))
-            let nib = UINib(nibName: "CardListCollectionView", bundle: bundle)
-            return nib.instantiate(withOwner: self, options: nil).first as? UIView
-        }
+//    required init?(coder aDecoder: NSCoder) {
+//            super.init(coder: aDecoder)
+//
+//            guard let view = loadViewFromNib() else { return }
+//            view.frame = self.bounds
+//            self.addSubview(view)
+//            contentView = view
+//        }
+//
+//        func loadViewFromNib() -> UIView? {
+//            let bundle = Bundle(for: type(of: self))
+//            let nib = UINib(nibName: "CardListCollectionView", bundle: bundle)
+//            return nib.instantiate(withOwner: self, options: nil).first as? UIView
+//        }
 
 
 }
