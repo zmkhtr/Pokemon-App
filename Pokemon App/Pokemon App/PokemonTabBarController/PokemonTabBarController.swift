3415fbfa511d493f6c46169241f2a91dc0d39d93
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-11-10 13:35:56 +0700

    Membuat PokemonTabBarController

---
+++ Pokemon App/Pokemon App/PokemonTabBarController/PokemonTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonTabBarController.swift
+//  Pokemon App
+//
+//  Created by Novan Agung Waskito on 09/11/22.
+//
+
+import Foundation
+import UIKit
+
+class PokemonTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //setting untuk TabBArController
+    }
+    
+}
