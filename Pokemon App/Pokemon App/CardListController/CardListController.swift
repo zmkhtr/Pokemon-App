208ab7cf461cfd66b6d5c14caa840f02548fdf56
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-11-12 17:50:45 +0700

    Create Card List Controller

---
+++ Pokemon App/Pokemon App/CardListController/CardListController.swift
@@ -0,0 +1,17 @@
+//
+//  CardListController.swift
+//  Pokemon App
+//
+//  Created by Novan Agung Waskito on 12/11/22.
+//
+
+import Foundation
+import UIKit
+
+class CardListContrller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
