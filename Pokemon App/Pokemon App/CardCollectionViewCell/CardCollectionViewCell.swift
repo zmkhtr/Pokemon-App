868d01bfe3c19593b969251ffbc943bc0a0b9b95
Nadia Darin <darinadia98@gmail.com>
2022-11-13 00:47:32 +0700

    Show error message and button if failed downloading image

--- Pokemon App/Pokemon App/CardCollectionViewCell/CardCollectionViewCell.swift
+++ Pokemon App/Pokemon App/CardCollectionViewCell/CardCollectionViewCell.swift
@@ -12,6 +12,8 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var pokemonCardImageView: UIImageView!
     @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var errorButton: UIButton!
     
     
     let imageLoader = ImageLoader()
@@ -19,7 +21,8 @@ class CardCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        setUpImageContainer()
+        setUpContainerAndError()
+        
     }
     
     func downloadImage(imageURL: String){
@@ -43,24 +46,31 @@ class CardCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    private func setUpImageContainer(){
+    private func setUpContainerAndError(){
         imageContainerView.layer.cornerRadius = 10
         imageContainerView.clipsToBounds = true
+        errorButton.titleLabel?.font = .systemFont(ofSize: 12.0, weight: .bold)
     }
     
     private func startDownloadImage(){
         imageContainerView.isShimmering = true
-        imageContainerView.backgroundColor = .gray
-
+        imageContainerView.backgroundColor = .darkGray
+        errorLabel.isHidden = true
+        errorButton.isHidden = true
     }
     
     private func successDownloadImage(_ downloadedImage: UIImage){
         pokemonCardImageView.image = downloadedImage
         imageContainerView.isShimmering = false
+        errorLabel.isHidden = true
+        errorButton.isHidden = true
     }
     
     private func failedDownloadImage(){
-        
+        imageContainerView.isShimmering = false
+        imageContainerView.backgroundColor = UIColor(named: "PrimaryColor")
+        errorLabel.isHidden = false
+        errorButton.isHidden = false
     }
     
 }
