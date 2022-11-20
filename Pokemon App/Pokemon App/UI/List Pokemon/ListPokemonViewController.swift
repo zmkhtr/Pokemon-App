8691f2386448225bf0f1267f3521e32339328e75
Mohammad Azri Khairuddin <azrimuhammad5@gmail.com>
2022-11-21 01:43:03 +0700

    Add networking and configure navbar and tabbar bacground when scrolling

--- Pokemon App/Pokemon App/UI/List Pokemon/ListPokemonViewController.swift
+++ Pokemon App/Pokemon App/UI/List Pokemon/ListPokemonViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 class ListPokemonViewController: UIViewController {
     
     @IBOutlet private var pokemonSearchBar: UISearchBar!
+    @IBOutlet private weak var listPokemonView: ListPokemonView!
+    private let pokemonLoader = PokemonLoaderImpl()
+    private let currentPage = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSearchBar()
+        retrieveListPokemon()
+        
+        listPokemonView.refreshData = { [weak self] in
+            self?.retrieveListPokemon()
+        }
     }
 
     private func configureSearchBar() {
@@ -21,5 +29,11 @@ class ListPokemonViewController: UIViewController {
         pokemonSearchBar.searchTextField.textColor = UIColor(named: "SearchBarTextColor")
         pokemonSearchBar.searchTextField.leftView?.tintColor = UIColor(named: "SearchBarTextColor")
         pokemonSearchBar.searchTextField.clearButtonMode = .never
+    }
+    
+    private func retrieveListPokemon() {
+        pokemonLoader.getListPokemon(currentPage: currentPage) { [weak self] result in
+            self?.listPokemonView.bindResult(result)
+        }
     }
 }
