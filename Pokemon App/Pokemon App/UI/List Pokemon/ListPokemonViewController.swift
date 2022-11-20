429bfb0ffbe7bde323b0a617db855babfd38b218
Mohammad Azri Khairuddin <azrimuhammad5@gmail.com>
2022-11-21 00:51:22 +0700

    Adding search bar to navigation bar title view

--- Pokemon App/Pokemon App/UI/List Pokemon/ListPokemonViewController.swift
+++ Pokemon App/Pokemon App/UI/List Pokemon/ListPokemonViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ListPokemonViewController.swift
+//  ViewController.swift
 //  Pokemon App
 //
 //  Created by PT.Koanba on 21/10/22.
@@ -10,18 +10,10 @@ import UIKit
 class ListPokemonViewController: UIViewController {
     
     @IBOutlet private var pokemonSearchBar: UISearchBar!
-    @IBOutlet private weak var listPokemonView: ListPokemonView!
-    private let pokemonLoader = PokemonLoaderImpl()
-    private let currentPage = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSearchBar()
-        retrieveListPokemon()
-        
-        listPokemonView.refreshData = { [weak self] in
-            self?.retrieveListPokemon()
-        }
     }
 
     private func configureSearchBar() {
@@ -29,11 +21,5 @@ class ListPokemonViewController: UIViewController {
         pokemonSearchBar.searchTextField.textColor = UIColor(named: "SearchBarTextColor")
         pokemonSearchBar.searchTextField.leftView?.tintColor = UIColor(named: "SearchBarTextColor")
         pokemonSearchBar.searchTextField.clearButtonMode = .never
-    }
-    
-    private func retrieveListPokemon() {
-        pokemonLoader.getListPokemon(currentPage: currentPage) { [weak self] result in
-            self?.listPokemonView.bindResult(result)
-        }
     }
 }
